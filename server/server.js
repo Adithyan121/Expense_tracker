0d764135a2dbd0c69042eadf6a0677cbe5624334
Adithyan G <97249089+Adithyan121@users.noreply.github.com>
2025-12-12 17:15:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: 'https://expense-tracker-6vs5.vercel.app/',
     credentials: true,
 }));
 
