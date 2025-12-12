3bf19c3dfc7c441c54efe8ed02b23cedc9d30ef3
Adithyan G <97249089+Adithyan121@users.noreply.github.com>
2025-12-12 17:27:26 +0530

    Fix CORS origin URL format

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://expense-tracker-6vs5.vercel.app/',
+    origin: 'https://expense-tracker-6vs5.vercel.app',
     credentials: true,
 }));
 
